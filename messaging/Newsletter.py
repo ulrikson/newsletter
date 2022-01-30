bb3bd93fefaa73fdfe25c34b1d4bcb07025815bc
ulrikson <erik.ulrikson@gmail.com>
2022-01-30 10:14:35 +0100

    adding olympics

--- messaging/Newsletter.py
+++ messaging/Newsletter.py
@@ -1,12 +1,14 @@
 from sources.Covid import Covid
 from sources.Omni import Omni
 from sources.Weather import Weather
+from sources.TV import TV
 
 class Newsletter:
     def __init__(self):
         self.covid = Covid().getHtml()
         self.omni = Omni().getHtml()
         self.weather = Weather("Stockholm", 3).getHtml()
+        self.tv = TV().getHtml()
 
     def createHTMLMessage(self):
         html = f"""\
@@ -15,6 +17,7 @@ class Newsletter:
                         {self.omni}
                         {self.weather}
                         {self.covid}
+                        {self.tv}
                     </body>
                 </html>
             """
---
+++ sources/TV.py
@@ -0,0 +1,6 @@
+class TV:
+    def __init__(self) -> None:
+        pass
+
+    def getHtml(self):
+        return '<h2>TV idag</h2><p><a href="https://www.tvmatchen.nu/vinter-os/">OS-program</a></p>'
