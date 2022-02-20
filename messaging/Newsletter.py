d11f23925afdc67c59e4417aa06fde3bbacaa34d
ulrikson <erik.ulrikson@gmail.com>
2022-02-20 16:02:33 +0100

    feat: removing covid

--- messaging/Newsletter.py
+++ messaging/Newsletter.py
@@ -1,11 +1,10 @@
-from sources.Covid import Covid
 from sources.Omni import Omni
 from sources.Weather import Weather
 from sources.TV import TV
 
+
 class Newsletter:
     def __init__(self):
-        self.covid = Covid().getHtml()
         self.omni = Omni().getHtml()
         self.weather = Weather("Stockholm", 3).getHtml()
         self.tv = TV().getHtml()
@@ -16,7 +15,6 @@ class Newsletter:
                     <body>
                         {self.omni}
                         {self.weather}
-                        {self.covid}
                         {self.tv}
                     </body>
                 </html>
